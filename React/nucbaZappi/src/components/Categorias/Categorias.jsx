43b7f8beb5ffb81f3b73b49eea352b9e2098d95e
GabrielNavas <cgnavas98@gmail.com>
2025-03-20 20:31:42 -0300

    nucbaZapi I

---
+++ React/nucbaZappi/src/components/Categorias/Categorias.jsx
@@ -0,0 +1,17 @@
+import { Categories } from "../../data/Categories";
+import Categoria from "./Categoria";
+import { CategoriasContainer } from "./CategoriasStyles";
+
+const Categorias = () => {
+  return (
+    <CategoriasContainer>
+      {
+        Categories.map((categoria) => (
+          <Categoria {...categoria} key={categoria.id}/>
+        ))
+      }
+    </CategoriasContainer>
+  );
+};
+
+export default Categorias;
